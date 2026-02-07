e4431f2c3fe6a3394b6f884008cc8f4337736711
ma7fouz81 <amedmafouz53@gmail.com>
2026-02-07 06:52:15 +0200

    Controllers

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,24 +3,51 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    function index()
+    public function index()
     {
         $data = Comment::all();
         return view('comment.index',['comments'=>$data , 'pageTitle'=>'Blog']);
     }
 
-    function create()
+    public function create()
     {
-//        Comment::create([
-//            'content'=>'This Is My Fourth Comment',
-//            'author'=>'Mahfouz',
-//            'post_id'=> 4
-//        ]);
-        Comment::factory(5)->create();
-        return redirect('/comments');
+        return view('comment.create',['pageTitle'=>'Blog - Create New Comment']);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        $comment =  Comment::findOrFail($id);
+        return view('comment.show' , ['comment'=>$comment , 'pageTitle'=>$comment->title]);
+    }
+
+    public function edit(string $id)
+    {
+        return view('comment.edit' ,['pageTitle'=>'Blog - Edit Comment']);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
